6126b0b87e32bd97a3fa9c5d62d1be4a9c9774e6
AisingioroHao0 <AisingioroHao0@outlook.com>
2023-09-12 19:35:34 +0800

    fix name

--- train/train_automatic_coloring_0.1.sh
+++ train/train_automatic_coloring_0.1.sh
@@ -1,17 +1,17 @@
 accelerate launch --multi_gpu --mixed_precision=fp16 train/train_sd_reference_only_automatic_coloring.py \
-    --pretrained_model_name_or_path='../DeepLearningContent/models/sd_reference_only/sd-2-1-init' \
     --ddp_find_unused_parameters \
-    --dataset_name='../DeepLearningContent/datasets/characters' \
+    --pretrained_model_name_or_path='/home/aihao/workspace/DeepLearningContent/models/sd_reference_only/sd-2-1-init' \
+    --dataset_name='/home/aihao/workspace/DeepLearningContent/datasets/characters' \
     --dataset_config_name='similar_pairs' \
-    --train_data_dir='../DeepLearningContent/datasets/characters' \
+    --train_data_dir='/home/aihao/workspace/DeepLearningContent/datasets/characters' \
     --load_dataset_num_proc=8 \
-    --cache_dir='../DeepLearningContent/datasets/characters/.cache' \
-    --output_dir="../DeepLearningContent/models/sd_reference_only/automatic_coloring_0.1" \
+    --output_dir="/home/aihao/workspace/DeepLearningContent/models/sd_reference_only/automatic_coloring_0.1" \
+    --tracker_project_name='automatic_coloring' \
     --train_batch_size=6 \
-    --max_train_steps=200000 \
+    --max_train_steps=500000 \
     --learning_rate=5e-5 \
     --checkpointing_steps=1000 \
-    --validation_steps=1000 \
+    --validation_steps=10 \
     --validation_prompt "validation_images/1/1.png" "validation_images/2/1.png" "validation_images/3/1.png" "validation_images/4/1.png" \
     --validation_blueprint "validation_images/1/2.png" "validation_images/2/2.png" "validation_images/3/2.png" "validation_images/4/2.png" \
     --use_8bit_adam \
@@ -21,4 +21,3 @@ accelerate launch --multi_gpu --mixed_precision=fp16 train/train_sd_reference_on
     --train_image_encoder \
     --resolution=256 \
     --resume_from_checkpoint="latest"
-# --load_dataset_streaming
