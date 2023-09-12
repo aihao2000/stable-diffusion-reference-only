f59bb947cb8d4046fafbc5a2dfb2d37b9279ebae
AisingioroHao0 <AisingioroHao0@outlook.com>
2023-09-12 18:17:48 +0800

    add automatic coloring

---
+++ train/train_automic_coloring_0.1.sh
@@ -0,0 +1,24 @@
+accelerate launch --multi_gpu --mixed_precision=fp16 train/train_sd_reference_only_automatic_coloring.py \
+    --pretrained_model_name_or_path='../DeepLearningContent/models/sd_reference_only/sd-2-1-init' \
+    --ddp_find_unused_parameters \
+    --dataset_name='../DeepLearningContent/datasets/characters' \
+    --dataset_config_name='similar_pairs' \
+    --train_data_dir='../DeepLearningContent/datasets/characters' \
+    --load_dataset_num_proc=8 \
+    --cache_dir='../DeepLearningContent/datasets/characters/.cache' \
+    --output_dir="../DeepLearningContent/models/sd_reference_only/automatic_coloring_0.1" \
+    --train_batch_size=6 \
+    --max_train_steps=200000 \
+    --learning_rate=5e-5 \
+    --checkpointing_steps=1000 \
+    --validation_steps=1000 \
+    --validation_prompt "validation_images/1/1.png" "validation_images/2/1.png" "validation_images/3/1.png" "validation_images/4/1.png" \
+    --validation_blueprint "validation_images/1/2.png" "validation_images/2/2.png" "validation_images/3/2.png" "validation_images/4/2.png" \
+    --use_8bit_adam \
+    --enable_xformers_memory_efficient_attention \
+    --seed 2221101 \
+    --mixed_precision=fp16 \
+    --train_image_encoder \
+    --resolution=256 \
+    --resume_from_checkpoint="latest"
+# --load_dataset_streaming
