8f358b6a62e5da5743e35707aa317b5fee91274c
AisingioroHao0 <AisingioroHao0@outllok.com>
2023-09-19 21:35:37 +0800

    rename

--- train/train_automatic_coloring_0.1.1_buaa.sh
+++ train/train_automatic_coloring_0.1.1_buaa.sh
@@ -1,10 +1,9 @@
-accelerate launch  --mixed_precision=fp16 --multi_gpu train/train_sd_reference_only_automatic_coloring.py \
+accelerate launch --mixed_precision=fp16 --multi_gpu train/train_sd_reference_only_automatic_coloring.py \
     --ddp_find_unused_parameters \
     --pretrained_model_name_or_path='/data/aihao/workspace/DeepLearningContent/models/sd_reference_only/automatic_coloring_0.1.1' \
     --dataset_name='/data/aihao/workspace/DeepLearningContent/datasets/characters' \
     --dataset_config_name='similar_pairs' \
     --train_data_dir='/data/aihao/workspace/DeepLearningContent/datasets/characters' \
-    --cache_dir="/data/aihao/workspace/DeepLearningContent/datasets/characters/.cache" \
     --load_dataset_num_proc=48 \
     --dataset_map \
     --dataloader_num_workers=12 \
@@ -16,12 +15,12 @@ accelerate launch  --mixed_precision=fp16 --multi_gpu train/train_sd_reference_o
     --learning_rate=5e-5 \
     --checkpointing_steps=1000 \
     --validation_steps=1000 \
-    --validation_prompt "validation_images/1/1.png" "validation_images/2/1.png" "validation_images/3/1.png" "validation_images/4/1.png" \
-    --validation_blueprint "validation_images/1/2.png" "validation_images/2/2.png" "validation_images/3/2.png" "validation_images/4/2.png" \
+    --validation_prompt "validation_images/1/1_capture.png" "validation_images/2/1_capture.png" "validation_images/3/1_capture.png" "validation_images/4/1_capture.png" \
+    --validation_blueprint "validation_images/1/2_line.png" "validation_images/2/2_line.png" "validation_images/3/2_line.png" "validation_images/4/2_line.png" \
     --use_8bit_adam \
     --enable_xformers_memory_efficient_attention \
     --seed 2221101 \
     --mixed_precision=fp16 \
     --train_image_encoder \
-    --resolution=256 \
-    --resume_from_checkpoint="latest"
+    --resolution=256
+# --resume_from_checkpoint="latest"
