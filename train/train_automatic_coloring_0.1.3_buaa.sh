97e40fbca882c5db9f4605acafdf3a0889412e9b
AisingioroHao0 <AisingioroHao0@outllok.com>
2023-09-21 19:00:51 +0800

    loss nan,add 0.1.3 train

---
+++ train/train_automatic_coloring_0.1.3_buaa.sh
@@ -0,0 +1,25 @@
+accelerate launch --mixed_precision=fp16 --multi_gpu train/train_sd_reference_only_automatic_coloring.py \
+    --ddp_find_unused_parameters \
+    --pretrained_model_name_or_path='/data/aihao/workspace/DeepLearningContent/models/sd_reference_only/automatic_coloring_0.1.2' \
+    --dataset_name='/data/aihao/workspace/DeepLearningContent/datasets/characters' \
+    --dataset_config_name='similar_pairs' \
+    --train_data_dir='/data/aihao/workspace/DeepLearningContent/datasets/characters' \
+    --load_dataset_num_proc=48 \
+    --dataloader_num_workers=12 \
+    --output_dir="/data/aihao/workspace/DeepLearningContent/models/sd_reference_only/automatic_coloring_0.1.3" \
+    --tracker_project_name='automatic_coloring' \
+    --report_to="tensorboard" \
+    --train_batch_size=10 \
+    --max_train_steps=200000 \
+    --learning_rate=1e-5 \
+    --checkpointing_steps=1000 \
+    --validation_steps=1000 \
+    --validation_prompt "validation_images/1/1_capture.png" "validation_images/2/1_capture.png" "validation_images/3/1_capture.png" "validation_images/4/1_capture.png" \
+    --validation_blueprint "validation_images/1/2_line.png" "validation_images/2/2_line.png" "validation_images/3/2_line.png" "validation_images/4/2_line.png" \
+    --use_8bit_adam \
+    --enable_xformers_memory_efficient_attention \
+    --seed 2221101 \
+    --mixed_precision=fp16 \
+    --train_image_encoder \
+    --resolution=512 \
+    --resume_from_checkpoint="latest"
