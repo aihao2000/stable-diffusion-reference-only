fa7978c61e8652cb42863efa34496c7c300a2603
AisingioroHao0 <AisingioroHao0@outllok.com>
2023-10-19 23:20:31 +0800

    delete useless files

--- labs/test_dataset.py
+++
@@ -1,7 +0,0 @@
-from datasets import load_dataset
-
-trin_dataset = load_dataset(
-    "/home/aihao/workspace/DeepLearningContent/datasets/images",
-    data_dir="/home/aihao/workspace/DeepLearningContent/datasets/images",
-    name="reference_only_for_automatic_coloring",
-)
